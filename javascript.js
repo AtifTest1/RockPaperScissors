dd7f2ff55be91b9a73b28805b4031c5183d18eee
Atif <ati_f160@hotmail.co.uk>
2022-06-19 13:46:31 +0100

    added ability to play single round

--- javascript.js
+++ javascript.js
@@ -17,3 +17,43 @@ function computerPlay()
     
 }
 
+function playRound(playerSelection, computerSelection)
+{
+    let result
+
+    if (playerSelection === computerSelection)
+    {
+        result = "This is a draw!"
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper")
+    {
+        result = "You lose! paper beats rock"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor")
+    {
+        result = "You win! rock beats scissor!"
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "rock")
+    {
+        result = "You win! paper beats rock!"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor")
+    {
+        result = "You lose! scissor beats paper!"
+    }
+
+    else if (playerSelection === "scissor" && computerSelection === "rock")
+    {
+        result = "You lose! rock beats scissor!"
+    }
+    else if (playerSelection === "scissor" && computerSelection === "paper")
+    {
+        result = "You win! scissor beats paper!"
+    }
+    return result
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
