15fdbe2d9b3da5378f0551beffc9046dfd3f5b6d
Atif <ati_f160@hotmail.co.uk>
2022-06-20 21:12:29 +0100

    added 5 round game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
+
+//This function randomly selects rock paper or scissor as the computer
 function computerPlay()
 {
-    let result = (Math.floor(Math.random() * 3))
+    let result = (Math.floor(Math.random() * 3));
 
     if (result === 0)
     {
@@ -10,50 +12,109 @@ function computerPlay()
     {
         result = "paper"
     }
-    else{
+    else
+    {
         result = "scissor"
     }
     return result;
     
 }
 
-function playRound(playerSelection, computerSelection)
+//store playscore for final tally
+let playerscore = 0
+let cpuscore = 0
+
+//This plays a single round of rock paper scissor
+function playRound()
 {
-    let result
+   
+    let playerwin = 0
+    let cpuwin = 0
+
+    playerSelection = prompt("rock paper or scissor?").toLowerCase();
+    computerSelection = computerPlay();
 
     if (playerSelection === computerSelection)
     {
-        result = "This is a draw!"
+        console.log("This is a draw! Nobody gets points")
     }
     else if (playerSelection === "rock" && computerSelection === "paper")
     {
-        result = "You lose! paper beats rock"
+        console.log("You lose! rock loses to paper")
+        cpuwin = cpuwin + 1
+        
     }
     else if (playerSelection === "rock" && computerSelection === "scissor")
     {
-        result = "You win! rock beats scissor!"
+        console.log("You win! rock beats scissor!")
+        playerwin = playerwin + 1
+        
     }
-
     else if (playerSelection === "paper" && computerSelection === "rock")
     {
-        result = "You win! paper beats rock!"
+        console.log("You win! paper beats rock!")
+        playerwin = playerwin + 1
+        
     }
     else if (playerSelection === "paper" && computerSelection === "scissor")
     {
-        result = "You lose! scissor beats paper!"
+        console.log("You lose! paper loses to scissors!")
+        cpuwin = cpuwin + 1
+        
     }
-
     else if (playerSelection === "scissor" && computerSelection === "rock")
     {
-        result = "You lose! rock beats scissor!"
+        console.log("You lose! scissor loses to rock!")
+        cpuwin = cpuwin + 1        
     }
     else if (playerSelection === "scissor" && computerSelection === "paper")
     {
-        result = "You win! scissor beats paper!"
+        console.log("You win! scissor beats paper!")
+        playerwin = playerwin + 1
     }
-    return result
+    
+    //player who wins get +1 points into their score
+    if(playerwin > cpuwin)
+    {
+        playerscore++
+        console.log("one point to player")
+    }
+    else if (playerwin < cpuwin)
+    {
+        cpuscore++
+        console.log("one point to cpu")
+    }
+    
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+
+//function loops playround 5 times, storing points into playscore or cpuscore
+function game()
+{   
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+
+    if (playerscore > cpuscore)
+    {
+        console.log("You have won! " + playerscore + " points!")
+    }
+    else if (playerscore < cpuscore)
+    {
+        console.log("The CPU has won " + cpuscore + " points!")
+    }
+    else
+    {
+        console.log("This is a draw")
+    }
+    
+}
+
+game()
+
+
+
+
+
+
